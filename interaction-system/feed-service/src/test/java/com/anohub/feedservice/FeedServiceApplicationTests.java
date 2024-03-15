ced37a9676b9ff89e118e522dc09faaf152cd956
Lera <prankerylove@gmail.com>
2024-03-15 20:29:46 +0300

    fixed feed service test bug

--- interaction-system/feed-service/src/test/java/com/anohub/feedservice/FeedServiceApplicationTests.java
+++ interaction-system/feed-service/src/test/java/com/anohub/feedservice/FeedServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.anohub.feedservice;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,9 +10,6 @@ class FeedServiceApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.assertDoesNotThrow(() -> {
-            FeedServiceApplication.main(new String[]{});
-        });
     }
 
 }
