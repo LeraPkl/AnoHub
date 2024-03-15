3baae3eedaf8990e25fd21c38aa4ad3cd32c89e2
Lera <prankerylove@gmail.com>
2024-03-15 23:04:53 +0300

    +private chat service

---
+++ interaction-system/private-chat-service/src/main/java/com/anohub/privatechatservice/PrivateChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anohub.privatechatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateChatServiceApplication.class, args);
+    }
+
+}
---
+++ interaction-system/private-chat-service/src/test/java/com/anohub/privatechatservice/PrivateChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anohub.privatechatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
