061e727f73cd881eff9800f70064bc6d0446156d
Lera <prankerylove@gmail.com>
2024-03-18 19:37:59 +0300

    fixed the bug with the env file :)

--- config-server/src/main/java/com/anohub/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/anohub/configserver/ConfigServerApplication.java
@@ -1,16 +1,25 @@
 package com.anohub.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApplication implements CommandLineRunner {
+
+    @Value("${GIT_PRIVATE_KEY}")
+    private String privateKey;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("PK IS: " + privateKey);
+    }
 }
 
