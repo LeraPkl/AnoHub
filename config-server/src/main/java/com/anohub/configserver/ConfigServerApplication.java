7857b44ae913c75e2b32741edc70e144b4825671
Lera <prankerylove@gmail.com>
2024-03-19 17:49:41 +0300

    changed git private key to PAT

--- config-server/src/main/java/com/anohub/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/anohub/configserver/ConfigServerApplication.java
@@ -1,15 +1,27 @@
 package com.anohub.configserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+@Slf4j
+public class ConfigServerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @Value("${GIT_PRIVATE_KEY}")
+    private String privateKey;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("pk is: {}", privateKey);
     }
 }
 
